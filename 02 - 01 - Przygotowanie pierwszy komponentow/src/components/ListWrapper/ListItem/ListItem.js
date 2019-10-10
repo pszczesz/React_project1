f867eae7bf20cdbf8036a4a0e9daaeaf8e67605d
Paweł Szczeszek <pszczesz@poczta.onet.pl>
2019-10-10 22:06:46 +0200

    list1

---
+++ 02 - 01 - Przygotowanie pierwszy komponentow/src/components/ListWrapper/ListItem/ListItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ListItem.css';
+const ListItem = () => (
+    <li className="listItemWrapper">
+        item1
+    </li>
+);
+export default ListItem;
---
+++ 02 - 01 - Przygotowanie pierwszy komponentow/src/components/ListWrapper/ListWrapper.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ListItem from './ListItem/ListItem';
+
+const ListWrapper = () => (
+    <ul>
+        <ListItem />
+    </ul>
+);
+export default ListWrapper;
