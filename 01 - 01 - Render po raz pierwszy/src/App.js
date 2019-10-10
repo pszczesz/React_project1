1cd27ee74514561519b53ab275c6f0a4d074b34e
Paweł Szczeszek <pszczesz@poczta.onet.pl>
2019-10-10 21:16:51 +0200

    01 - finish

---
+++ 01 - 01 - Render po raz pierwszy/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const App = ()=>(
+    <div className="wrapper">
+        <h1  className="mainHeader">Hello world</h1>
+        <h2 className="secondaryHeader">hello from eduweb</h2>
+    </div>
+    );
+   export default App;
