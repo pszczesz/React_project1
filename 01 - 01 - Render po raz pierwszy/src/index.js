cb10d45dbecaedfacfbac13ebb85f7744fba2102
Paweł Szczeszek <pszczesz@poczta.onet.pl>
2019-10-10 20:43:51 +0200

    rr

--- 01 - 01 - Render po raz pierwszy/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const dd = new Date().toLocaleString();
-ReactDOM.render(
-    <h1>Hello world dd</h1>,
-    document.querySelector("#root")
-);
