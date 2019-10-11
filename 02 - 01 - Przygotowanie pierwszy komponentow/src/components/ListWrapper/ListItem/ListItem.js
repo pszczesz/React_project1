050e0984f9393af4c091f372bd4675755ad57829
Paweł Szczeszek <pszczesz@poczta.onet.pl>
2019-10-11 22:24:35 +0200

    02-01 list

--- 02 - 01 - Przygotowanie pierwszy komponentow/src/components/ListWrapper/ListItem/ListItem.js
+++ 02 - 01 - Przygotowanie pierwszy komponentow/src/components/ListWrapper/ListItem/ListItem.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './ListItem.css';
-const ListItem = () => (
-    <li className="listItemWrapper">
-        item1
-    </li>
-);
+// import danAbramovImage from '../../../assets/images/danabramov.jpg';
+const ListItem = ({
+    name,
+    description,
+    image,
+    twitterLink
+}) => (
+        <li className="listItem__wrapper">
+            <img
+                src={image} alt={name}
+                className="listItem__image"
+            />
+            <div>
+                <h2 className="listItem__name">{name}</h2>
+                <p className="listItem__description">{description}</p>
+                <a className="listItem__button" href={twitterLink}>visit twitter page</a>
+            </div>
+        </li>
+    );
 export default ListItem;
--- 02 - 01 - Przygotowanie pierwszy komponentow/src/components/ListWrapper/ListWrapper.js
+++ 02 - 01 - Przygotowanie pierwszy komponentow/src/components/ListWrapper/ListWrapper.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
+import './ListWrapper.css';
+import { twitterAccounts } from '../../data/twitterAccounts';
+
+
 
 const ListWrapper = () => (
-    <ul>
-        <ListItem />
+    <ul className="listWrapper_wrapper">
+        {twitterAccounts.map(item => (
+            <ListItem key={item.name}  {...item}/>))}
+
     </ul>
 );
 export default ListWrapper;
