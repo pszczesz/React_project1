e33e51f2f5fe46605cb58a347cdc67acef337766
pszczesz <pszczesz@gmail.com>
2019-11-21 14:34:18 +0100

    d5

--- reactPro16/todo/src/App.js
+++ reactPro16/todo/src/App.js
@@ -37,8 +37,8 @@ export default class App extends Component {
       newItemText: ""
     }
   }
-  updateNewTextValue = (event)=>{
-    this.setState({newItemText: event.target.value });
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
   }
   createNewToDo = () => {
     if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
@@ -46,7 +46,8 @@ export default class App extends Component {
         todoItems: [...this.state.todoItems,
         { action: this.state.newItemText, done: false }],
         newItemText: ""
-      })
+      });
+      console.log(this.state.todoItems)
     }
   }
   changeStateData = () => {
@@ -54,22 +55,43 @@ export default class App extends Component {
       userName: this.state.userName === "Adam" ? "Bob" : "Adam"
     })
   }
+
+  toggleTodo = (todo) => this.setState({
+    todoItems:
+      this.state.todoItems.map(item => item.action === todo.action
+        ? { ...item, done: !item.done } : item)
+  });
+  todoTableRows = () => this.state.todoItems.map(item =>
+    <tr key={item.action}>
+      <td>{item.action}</td>
+      <td>
+        <input type="checkbox" checked={item.done}
+          onChange={() => this.toggleTodo(item)} />
+      </td>
+    </tr>);
+
   render = () =>
     <div>
       <h4 className="bg-primary text-white p-2 text-center">
         {this.state.userName}'s
         To Do List
-        ({this.state.todoItems.filter(t=>!t.done).length} items to do)
+        ({this.state.todoItems.filter(t => !t.done).length} items to do)
       </h4>
       <div className="container-fluid">
         <div className="my-1">
           <input className="form-control"
-          value={this.state.newItemText}
-          onChange={this.updateNewTextValue} />
+            value={this.state.newItemText}
+            onChange={this.updateNewTextValue} />
           <button className="btn btn-primary mt-1"
-                  onClick={this.createNewToDo}>Add</button>
+            onClick={this.createNewToDo}>Add</button>
         </div>
+        <table className="table table-striped table-bordered">
+                    <thead>
+                        <tr><th>Description</th><th>Done</th></tr>
+                    </thead>
+                    <tbody>{ this.todoTableRows() }</tbody>
+                </table>
       </div>
-     
+
     </div>
 }
