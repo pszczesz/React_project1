aad1365bed471d7b64e3b8de6900ac6d880153b3
pszczesz <pszczesz@gmail.com>
2019-11-21 12:31:39 +0100

    d4

---
+++ reactPro16/todo/src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Adam",
+      todoItems: [{ action: "Buy Flowers", done: false },
+      { action: "Get Shoes", done: false },
+      { action: "Collect Tickets", done: true },
+      { action: "Call Joe", done: false },
+      ],
+      newItemText: ""
+    }
+  }
+  updateNewTextValue = (event)=>{
+    this.setState({newItemText: event.target.value });
+  }
+  createNewToDo = () => {
+    if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
+      this.setState({
+        todoItems: [...this.state.todoItems,
+        { action: this.state.newItemText, done: false }],
+        newItemText: ""
+      })
+    }
+  }
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Adam" ? "Bob" : "Adam"
+    })
+  }
+  render = () =>
+    <div>
+      <h4 className="bg-primary text-white p-2 text-center">
+        {this.state.userName}'s
+        To Do List
+        ({this.state.todoItems.filter(t=>!t.done).length} items to do)
+      </h4>
+      <div className="container-fluid">
+        <div className="my-1">
+          <input className="form-control"
+          value={this.state.newItemText}
+          onChange={this.updateNewTextValue} />
+          <button className="btn btn-primary mt-1"
+                  onClick={this.createNewToDo}>Add</button>
+        </div>
+      </div>
+     
+    </div>
+}
